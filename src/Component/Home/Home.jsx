f57f360ad5defbb866b3ecf0575c54af951c9ce7
floriane6027 <lunettafloriane24@gmail.com>
2023-10-12 10:45:43 +0200

    test3

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -3,7 +3,7 @@ import Menu from "../Menu/Menu";
 import InputPost from "../InputPost/InputPost";
 import Card from "../Card/Card";
 import { useEffect, useState } from "react";
-
+// test git
 function Home() {
   return (
     <>

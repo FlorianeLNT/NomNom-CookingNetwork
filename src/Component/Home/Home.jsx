988b553b7bf502d6f90c116808f99ece9c2b12d8
KevinRMO <romanokevin1992@gmail.com>
2023-10-12 11:46:14 +0200

    test4

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,9 +2,14 @@ import "./Home.css";
 import Menu from "../Menu/Menu";
 import InputPost from "../InputPost/InputPost";
 import PostedCard from "../PostedCard/PostedCard";
+import SignUp from "../Signup/Signup";
 import { useEffect, useState } from "react";
 
 function Home() {
-  return <></>;
+  return (
+    <>
+      <SignUp />
+    </>
+  );
 }
 export default Home;
